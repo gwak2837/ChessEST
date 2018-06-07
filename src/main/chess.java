346392be6a4c3911c48987cc68f07b1abb63b5a8
곽태욱 <33145190+rmfpdlxmtidl@users.noreply.github.com>
2018-06-08 01:47:17 +0900

    Create chess.java

---
+++ src/main/chess.java
@@ -0,0 +1,12 @@
+package main;
+
+import chessFrame.Frame;
+
+public class Chess {
+    public static void main(String[] args) {
+        new Frame();
+    }
+    public static String getTitle() {
+        return " MyChess ";
+    }
+}
